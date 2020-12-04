2adec34c8f3c23cde8c378e0da023f80209a540e
jovanovic-nemanja <69627607+jovanovic-nemanja@users.noreply.github.com>
2020-12-05 03:33:49 +0900

    api for discounts

--- backend/app/Http/Controllers/API/SwiftApiController.php
+++ backend/app/Http/Controllers/API/SwiftApiController.php
@@ -113,17 +113,19 @@ class SwiftApiController extends Controller
      */
     public function getDiscountlistsByVendor(Request $request)
     {
-        if (@$request->vendor_id) {
+        if (@$request->category_id) {
             $result = DB::table('discounts')
                             ->join('vendors', 'vendors.id', '=', 'discounts.vendor_id')
                             ->join('categories', 'categories.id', '=', 'vendors.category_id')
-                            ->where('discounts.vendor_id', $request->vendor_id)
+                            ->where('vendors.category_id', $request->category_id)
+                            ->where('vendors.vendorname', 'like', '%'.$request->word.'%')
                             ->select('discounts.*', 'vendors.vendorname', 'vendors.location', 'vendors.photo', 'vendors.email', 'vendors.phone', 'discounts.sign_date as discounts_date', 'categories.category_name')
                             ->get();
         }else{
             $result = DB::table('discounts')
                             ->join('vendors', 'vendors.id', '=', 'discounts.vendor_id')
                             ->join('categories', 'categories.id', '=', 'vendors.category_id')
+                            ->where('vendors.vendorname', 'like', '%'.$request->word.'%')
                             ->select('discounts.*', 'vendors.vendorname', 'vendors.location', 'vendors.photo', 'vendors.email', 'vendors.phone', 'discounts.sign_date as discounts_date', 'categories.category_name')
                             ->get();
         }
