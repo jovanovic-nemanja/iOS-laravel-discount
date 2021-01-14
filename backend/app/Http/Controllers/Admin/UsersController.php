958db023cacd8281dd3a3cc35db2e963839d6e73
jovanovic-nemanja <69627607+jovanovic-nemanja@users.noreply.github.com>
2021-01-14 16:21:37 +0900

    a

--- backend/app/Http/Controllers/Admin/UsersController.php
+++ backend/app/Http/Controllers/Admin/UsersController.php
@@ -20,7 +20,7 @@ use Illuminate\Support\Facades\Validator;
 class UsersController extends Controller
 {
     public function __construct(){
-        $this->middleware(['auth', 'admin'])->except(['store', 'loginUser', 'logout', 'emailverify', 'validateCode', 'forgotpassword', 'resetpwd', 'resetUserpassword', 'updateAccount', 'loginUserwithApple']);
+        $this->middleware(['auth', 'admin'])->except(['store', 'loginUser', 'logout', 'emailverify', 'validateCode', 'forgotpassword', 'resetpwd', 'resetUserpassword', 'updateAccount', 'loginUserwithApple', 'updateAppleAccount']);
     }
 
     /**
